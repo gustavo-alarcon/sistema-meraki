5212b6029d8adc897ff3228ccdc7a7fa0acfbe83
Gustavo <galarcon@mearki-s.com>
2019-06-29 23:18:47 -0500

    advance feature/OP-form

---
+++ src/app/main/sales/orders/orders-form/orders-form-save-dialog/orders-form-save-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-form-save-dialog',
+  templateUrl: './orders-form-save-dialog.component.html',
+  styles: []
+})
+export class OrdersFormSaveDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
